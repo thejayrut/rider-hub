7ea390caf607925b3272f6a648dd0090dfeb62ed
thejayrut <145215841+thejayrut@users.noreply.github.com>
2026-08-21 19:54:45 +0000

    Wire Phase 3 persistent Rider Hub provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { RiderHubProvider } from './store/RiderHubProvider'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RiderHubProvider>
+      <App />
+    </RiderHubProvider>
   </StrictMode>
 )
