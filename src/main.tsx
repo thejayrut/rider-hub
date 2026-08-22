e58ef98b33df54c9441ea1dda97c48679ed550c5
thejayrut <145215841+thejayrut@users.noreply.github.com>
2026-08-22 06:18:45 +0000

    Load Phase 3 polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { RiderHubProvider } from './store/RiderHubProvider'
 import './styles.css'
+import './phase3-polish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
