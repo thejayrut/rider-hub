935d6948cf194ebed86e4fa2ba7e2f2e12c7fe0c
thejayrut <145215841+thejayrut@users.noreply.github.com>
2026-08-23 07:44:51 +0000

    Bump stable cache after mobile sign-in fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='riderhub-stable-v33';
+const CACHE='riderhub-stable-v34';
 const ASSETS=['./','./index.html','./stable-app.css','./stable-compat.css','./stable-app.js','./stable-auth.js','./firebase-config.js','./motorcycle-catalog.js','./manual-reader.js','./cloud-drive-v2.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg','./icon-maskable.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
